2287d264100d1dbbbd7313c5efc4c4d0e04d6a18
Enes Gök <enesgok@Enes-MBP.home>
2022-03-19 22:46:38 +0300

    created backend

---
+++ index.js
@@ -0,0 +1,18 @@
+const express=require("express");
+const app=express();
+const moongose=require("mongoose");
+const dotenv=require("dotenv");
+const userRoute=require("./router/user")
+
+dotenv.config();
+
+moongose.connect(process.env.MONGO_URL)
+.then(()=>console.log("DB Connect is successfull"))
+.catch((err)=>console.log(err));
+
+app.use(express.json());
+app.use("/api/users",userRoute);
+
+app.listen(process.env.PORT || 5000,()=>{
+    console.log("backend server is running")
+})
