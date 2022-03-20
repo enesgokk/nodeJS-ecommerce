9214698ec9a6b52e26117c7a91722645a16b00ba
Enes Gök <enesgok@Enes-MBP.home>
2022-03-20 15:39:46 +0300

    Added JWT Token

--- router/auth.js
+++ router/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt=require("jsonwebtoken");
 
 //REGISTER
 
@@ -33,9 +34,17 @@ router.post("/login",async (req,res)=>{
         const OriginalPassword=hashedPassword.toString(CryptoJS.enc.Utf8)
         OriginalPassword!==req.body.password && res.status(401).json("Wrong credentials!");
 
+        const accessToken=jwt.sign({
+            id:user._id,
+            isAdmin:user.isAdmin
+        },
+        process.env.JWT_SEC,
+        {expiresIn:"3d"}
+        )
+
         const {password, ...others}=user._doc; //password dont reveal in res
 
-        res.status(200).json(others);
+        res.status(200).json({...others,accessToken});
 
     } catch (err) {
         res.status(500).json(err)
