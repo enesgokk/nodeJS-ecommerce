50cd476c95dfdc80a33ca1e27ee1dc11bc2e2f92
Enes Gök <enesgok@Enes-MacBook-Pro.local>
2022-04-17 16:58:19 +0300

    created order and prooduct r00ter

--- models/Order.js
+++ models/Order.js
@@ -1,23 +1,23 @@
 const mongoose = require("mongoose");
 
 const OrderSchema = new mongoose.Schema({
-    userId: { type: String, required: true, unique: true },
+    userId: { type: String, required: true, unique:false },
     products: [
         {
-            productId:{
-                type:String,
+            productId: {
+                type: String,
             },
-            quantity:{
-                type:Number,
-                default:1,
+            quantity: {
+                type: Number,
+                default: 1,
             },
         },
     ],
-    amount:{type:Number, required:true},
-    adress:{type:Object, required:true},
-    status:{type:String, default:"pending"}
+    amount: { type: Number, required: true },
+    address: { type: Object, required: true },
+    status: { type: String, default: "pending" }
 },
     { timestamps: true }
 );
 
-module.exports=mongoose.model("Order",OrderSchema);
+module.exports = mongoose.model("Order", OrderSchema);
